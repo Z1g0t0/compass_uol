600ad60552bf205373d92c377faeffa1d4b1a1a7
Z1g0t0 <eduardo.suzuki8@hotmail.com>
2023-06-22 07:33:29 -0300

    Adicionado certificados da sprint 2

--- Sprint_2/BaseDados/Loja/Ex_2.sql
+++
@@ -1,5 +0,0 @@
-select tbvendas.cdpro, nmcanalvendas, nmpro, qtd as quantidade_vendas
-from tbvendas
-left join tbestoqueproduto on tbvendas.cdpro = tbestoqueproduto.cdpro
-group by tbvendas.cdpro
-limit 10
